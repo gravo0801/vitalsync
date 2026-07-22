7de9e07531d8e11832a661909029c67fcced7c86
gravo0801 <fnaticdoc@gmail.com>
2026-07-22 21:33:49 +0900

    feat: add workout history API (lib/firebaseConfig.ts)

---
+++ lib/firebaseConfig.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBCJ8VBLzx_VmOyamqmJfI99xjVTuMlz4E",
+  authDomain: "vitalsync-8c169.firebaseapp.com",
+  projectId: "vitalsync-8c169",
+  storageBucket: "vitalsync-8c169.firebasestorage.app",
+  messagingSenderId: "845233550940",
+  appId: "1:845233550940:web:3a8b7ad5c4ece10b3dd899",
+  measurementId: "G-SVKZN6D9NH",
+} as const;
+
+export const PERSONAL_USER_ID = "personal-user";
